d08a6687f72d75f1ee8762329b548e61aba7c20a
sharpirate <stoynov.bogomil@gmail.com>
2021-09-05 18:36:36 +0300

    create main timeline

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
         </Head>
-        <body>
+        <body className="bg-primary-300">
           <Main />
           <NextScript />
         </body>

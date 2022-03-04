6be0d442cf6e8b2bf5d61bdf06d188765bb38f23
sharpirate <stoynov.bogomil@gmail.com>
2022-03-04 21:40:43 +0200

    fix last interval

--- components/molecules/MainTimeline.js
+++ components/molecules/MainTimeline.js
@@ -79,7 +79,13 @@ function MainTimeline() {
         
       } else {
         const nextIntervalIndex = timeline.intervals.findIndex(interval => interval.timestamp > Date.now());
-        setActiveInterval(timeline.intervals[nextIntervalIndex - 1]);
+
+        // check if we are already at the last interval
+        if (nextIntervalIndex === -1) {
+          setActiveInterval(timeline.intervals.pop());
+        } else {
+          setActiveInterval(timeline.intervals[nextIntervalIndex - 1]);
+        }
       }
     }
   }

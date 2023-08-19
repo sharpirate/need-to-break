9ee85ca4ce9e46e3759c4fdcc72aeabc0d2112e9
Sharpirate <stoynov.bogomil@gmail.com>
2023-08-18 22:51:58 -0700

    remove deleted pages

--- pages/flexible.js
+++
@@ -1,7 +0,0 @@
-import CardsLayout, { cardsLayoutTypes } from "../components/molecules/cards/CardsLayout";
-
-function FlexiblePage() {
-  return <CardsLayout type={cardsLayoutTypes.flexible} />
-}
-
-export default FlexiblePage;
